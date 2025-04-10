10851f96b01269ed59361a8e7a6d35c1f01d7422
Jordan Williams <jordan88w28@gmail.com>
2025-04-10 13:29:16 -0500

    Update pandas-transforming-dataframes.py

--- pandas/pandas-transforming-dataframes.py
+++ pandas/pandas-transforming-dataframes.py
@@ -333,3 +333,18 @@ print(result)
 28                Nevada         23.314
 47            Washington         21.829
 32              New York         20.392
+
+
+##Counting categorical variables
+# Count the number of stores of each store type in store_types.
+
+
+# Count the proportion of stores of each store type in store_types.
+
+
+# Count the number of stores of each department in store_depts, sorting the counts in descending order.
+
+
+# Count the proportion of stores of each department in store_depts, sorting the proportions in descending order.
+
+
